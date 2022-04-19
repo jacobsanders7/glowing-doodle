a52022ec94c666a35844064d66344035d6926d33
jacobsanders7 <97248244+jacobsanders7@users.noreply.github.com>
2022-04-19 14:08:36 -0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+
+function ClearFields() {
+
+    document.getElementById("card").value = "";
+    document.getElementById("security").value = "";
+    document.getElementById("expire").value = "";
+    document.getElementById("date").value = "";
+}
+
+
+function ClearText() {
+
+    document.getElementById("fname").value = "";
+    document.getElementById("lname").value = "";
+    document.getElementById("Phone").value = "";
+    document.getElementById("SSN").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("password").value = "";
+}
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+
+        
+
+        
+      
+    
+  
+
+});
---
+++ scripts1.js
@@ -0,0 +1,7 @@
+var Student =
+{
+    name : "Jacob Sanders",
+    age : 22,
+    dept : "2 Years",
+    score : "Senior" 
+};
